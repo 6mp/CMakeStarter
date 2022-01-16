60ab5819c6dab0482572a58150adfa75d783e1ca
matt <30198937+6mp@users.noreply.github.com>
2022-01-15 23:40:53 -0500

    Delete cmake-build-release directory

--- cmake-build-release/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/matt/CLionProjects/CMakeStarter/test
-# Build directory: /home/matt/CLionProjects/CMakeStarter/cmake-build-release/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/matt/CLionProjects/CMakeStarter/cmake-build-release/test/Tests_include-b12d07c.cmake")
-subdirs("../_deps/catch2-build")
